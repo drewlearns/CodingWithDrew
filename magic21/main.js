13472df5d1128daaa1706d75e6285ca78bdd660c
drewlearns <drewlearns@hey.com>
2022-07-25 11:47:55 -0400

    fixed it maybe?

--- magic21/main.js
+++ magic21/main.js
@@ -119,15 +119,15 @@ cardList1.addEventListener(
             cardlist3.innerHTML = ""
             // deal kinda
             for (i = 0; i < 7; i++) {
-                group22.push(newdeck2[g2[i]]);
                 group21.push(newdeck2[g1[i]]);
+                group22.push(newdeck2[g2[i]]);
                 group23.push(newdeck2[g3[i]]);
             }
             for (i = 0; i < group21.length; i++) {
-                cardList1.innerHTML += `<li class="card display"><span class="top">${group22[i]}</span><span class="cardType">🧜‍♀️</span><span class="mirror">🧜‍♀️</span><span class="bottom">${group22[i]}</span></li>`
+                cardList2.innerHTML += `<li class="card display"><span class="top">${group21[i]}</span><span class="cardType">🧜‍♀️</span><span class="mirror">🧜‍♀️</span><span class="bottom">${group21[i]}</span></li>`
             }
             for (i = 0; i < group22.length; i++) {
-                cardList2.innerHTML += `<li class="card display"><span class="top">${group21[i]}</span><span class="cardType">🧜‍♀️</span><span class="mirror">🧜‍♀️</span><span class="bottom">${group21[i]}</span></li>`
+                cardList1.innerHTML += `<li class="card display"><span class="top">${group22[i]}</span><span class="cardType">🧜‍♀️</span><span class="mirror">🧜‍♀️</span><span class="bottom">${group22[i]}</span></li>`
             }
             for (i = 0; i < group23.length; i++) {
                 cardList3.innerHTML += `<li class="card display"><span class="top">${group23[i]}</span><span class="cardType">🧜‍♀️</span><span class="mirror">🧜‍♀️</span><span class="bottom">${group23[i]}</span></li>`
